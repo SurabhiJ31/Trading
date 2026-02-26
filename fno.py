c6181e6d8bd64b064960c061fee51aa9a087f8db
Surabhi Jain <surabhij.jain10@gmail.com>
2026-02-26 10:22:13 +0400

    updated fno request

--- fno.py
+++ fno.py
@@ -1,14 +1,38 @@
 import nsefin
 import streamlit as st
 from service_provider import get_companies_service
+import requests
 
 comp_service=get_companies_service()
 
-@st.cache_data
+
+def get_nse_session():
+    session = requests.Session()
+    session.headers.update({
+        "User-Agent": "Mozilla/5.0",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Encoding": "gzip, deflate, br"
+    })
+    session.get("https://www.nseindia.com")  # Important: initialize cookies
+    return session
+
+GLOBAL_NSE_SESSION=get_nse_session()
+@st.cache_data(ttl="1d")
 def get_fno_list():
-    nse = nsefin.NSEClient()
-    fno_stocks = nse.get_fno_list()
-    return list(fno_stocks['symbol'])
+    session = GLOBAL_NSE_SESSION
+    url = "https://www.nseindia.com/api/equity-stockIndices?index=SECURITIES%20IN%20F%26O"
+    
+    response = session.get(url)
+    data = response.json()
+    
+    stocks = [item["symbol"] for item in data["data"]]
+    return stocks
+
+# @st.cache_data
+# def get_fno_list():
+#     nse = nsefin.NSEClient()
+#     fno_stocks = nse.get_fno_list()
+#     return list(fno_stocks['symbol'])
 
 #company is symbol
 def has_fno(company):
